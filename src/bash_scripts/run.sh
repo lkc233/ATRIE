10651acd55480205b39eae64b6ad0f14b4676af8
lkc <luokangcheng@stu.pku.edu.cn>
2025-04-14 09:05:18 +0000

    update filename

--- src/bash_scripts/run.sh
+++ src/bash_scripts/run.sh
@@ -12,13 +12,13 @@ sleep 3m
 python src/data_preparation/llm_data_filter.py --model Qwen/Qwen2.5-72B-Instruct --input_folder data/docs/v0/Qwen2.5-7B-Instruct_filtered
 # 20h
 python src/data_preparation/data_split.py
-python src/generate_summary/extract_reason.py --input_dir data/docs/v1/test --output_dir data/docs/v1/test/reason
-python src/generate_summary/extract_reason.py --input_dir data/docs/v1/train --output_dir data/docs/v1/train/reason
+python src/generate_interpretation/extract_reason.py --input_dir data/docs/v1/test --output_dir data/docs/v1/test/reason
+python src/generate_interpretation/extract_reason.py --input_dir data/docs/v1/train --output_dir data/docs/v1/train/reason
 bash src/bash_scripts/stop_qwen_72b_servers.sh
 sleep 15
 bash src/bash_scripts/vllm_Qwen72B_server_7280_long.sh
 sleep 3m
-python src/generate_summary/generate_summary.py
+python src/generate_interpretation/generate_interpretation.py
 tmux kill-session -t vllm_Qwen72B_server_7280_long
 sleep 15
 bash src/bash_scripts/start_qwen_72b_servers.sh
